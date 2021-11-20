b7d1f7cf70844fed0632ff0b2f263d83769d099d
Sevvalnurdonmezler <72507223+Sevvalnurdonmezler@users.noreply.github.com>
2021-11-20 11:56:35 +0300

    Add files via upload

---
+++ Ders_3.1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace Ders_3._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_number1 = new System.Windows.Forms.TextBox();
+            this.txt_number2 = new System.Windows.Forms.TextBox();
+            this.btn_hesapla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(50, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sayı 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(50, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sayı 2";
+            // 
+            // txt_number1
+            // 
+            this.txt_number1.Location = new System.Drawing.Point(104, 63);
+            this.txt_number1.Name = "txt_number1";
+            this.txt_number1.Size = new System.Drawing.Size(100, 23);
+            this.txt_number1.TabIndex = 2;
+            // 
+            // txt_number2
+            // 
+            this.txt_number2.Location = new System.Drawing.Point(104, 114);
+            this.txt_number2.Name = "txt_number2";
+            this.txt_number2.Size = new System.Drawing.Size(100, 23);
+            this.txt_number2.TabIndex = 2;
+            // 
+            // btn_hesapla
+            // 
+            this.btn_hesapla.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_hesapla.Location = new System.Drawing.Point(60, 163);
+            this.btn_hesapla.Name = "btn_hesapla";
+            this.btn_hesapla.Size = new System.Drawing.Size(144, 38);
+            this.btn_hesapla.TabIndex = 3;
+            this.btn_hesapla.Text = "Hesapla";
+            this.btn_hesapla.UseVisualStyleBackColor = true;
+            this.btn_hesapla.Click += new System.EventHandler(this.btn_hesapla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_hesapla);
+            this.Controls.Add(this.txt_number2);
+            this.Controls.Add(this.txt_number1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Ders_3.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_number1;
+        private System.Windows.Forms.TextBox txt_number2;
+        private System.Windows.Forms.Button btn_hesapla;
+    }
+}
+
---
+++ Ders_3.1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ders_3._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_hesapla_Click(object sender, EventArgs e)
+        {
+            decimal sayi1 = Convert.ToInt32(txt_number1.Text);
+            decimal sayi2 = Convert.ToInt32(txt_number2.Text);
+            decimal hesap_top = sayi1 + sayi2;
+            if (hesap_top>50) 
+            {
+                MessageBox.Show("Girdiğiniz Sayı 50'den (" +hesap_top+" ) büyüktür.Geçtiniz...");
+            }
+            else if (hesap_top <30)
+            {
+                MessageBox.Show("Bütünlemeye Kaldınız....  ( " + hesap_top + " )");
+            }
+            
+            else
+            {
+                MessageBox.Show("Sayınız 50'den küçüktür. ("+hesap_top+ " ) Kaldınız...");
+            }
+        }
+    }
+}
